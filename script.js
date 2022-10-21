d2cd0a7176300dc57ff9207ffb0d38652879013a
Igor Holanda <oigorholanda@gmail.com>
2022-10-20 22:23:17 -0300

    FInalizado com cronometro

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gifs = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+const gifs = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
 let numCartas = 1;
 let cliques = 0;
 let indice1;
@@ -6,11 +6,13 @@ let indice2;
 let primeiraCarta;
 let segundaCarta;
 let acertos = 0;
+let temp = 0;
+let Cronid;
 
 
 //Perguntar e gerar o numero de cartas
 function iniciarJogo() {
-    while ((numCartas%2 !== 0 || numCartas < 4 || numCartas > 20)) {
+    while ((numCartas % 2 !== 0 || numCartas < 4 || numCartas > 20)) {
         numCartas = prompt('Escolha uma quantidade PAR de cartas entre 4 e 20');
     }
     //Igualar os gifs ao numero de cartas
@@ -20,10 +22,11 @@ function iniciarJogo() {
     //Armazenar o elemento lista na variavel lista e limpa o html da lista
     const lista = document.querySelector('.lista');
     lista.innerHTML = "";
+    lista.innerHTML = `<div class="cronometro"></div>`
 
     //Insere as cartas formatadas com gifs 2 a 2
-    for (let i=0; i<numCartas; i++) {
-    lista.innerHTML += `
+    for (let i = 0; i < numCartas; i++) {
+        lista.innerHTML += `
     <li onclick="FazerAcao(this)">
     <div id=${gifs[i]} class="card">
         <div class="carta">
@@ -36,15 +39,27 @@ function iniciarJogo() {
     </li> 
     `;
     }
+    iniciaCronometro();
 }
 
 // Função para o embaralhador
-function comparador() { 
-	return Math.random() - 0.5; 
+function comparador() {
+    return Math.random() - 0.5;
 }
 
+function iniciaCronometro() {
+    Cronid = setInterval(atualizaCronometro, 1000);
+}
+
+function atualizaCronometro(){
+    const cron = document.querySelector('.cronometro') 
+    cron.innerHTML = `${temp++}s`;
+}
+
+
+
 //Virar somente a carta clicada e armazenar ID
-function FazerAcao (item) {
+function FazerAcao(item) {
     
     if (item.classList.contains('clicada')) {
         return;
@@ -54,24 +69,25 @@ function FazerAcao (item) {
         indice1 = item.querySelector('div').id;
         item.classList.add('clicada');
         console.log(indice1);
+        cliques++;
 
     } else if (segundaCarta === undefined) {
         segundaCarta = item
         indice2 = item.querySelector('div').id;
         item.classList.add('clicada');
         console.log(indice2);
+        cliques++;
 
         compararCartas();
-    } 
+    }
 }
 
 //Compara os cards e define o acerto ou reset
 function compararCartas() {
-    cliques ++;
     if (indice1 === indice2) {
         console.log('Acertou');
 
-        contadorDeAcertos ();
+        contadorDeAcertos();
 
     } else {
         console.log('Errou');
@@ -81,7 +97,7 @@ function compararCartas() {
 }
 
 //Desvira o card em caso de erro
-function removerClasse () {
+function removerClasse() {
     primeiraCarta.classList.remove('clicada');
     segundaCarta.classList.remove('clicada');
     primeiraCarta = undefined;
@@ -89,24 +105,25 @@ function removerClasse () {
 }
 
 //Conta os acertos e finaliza o jogo
-function contadorDeAcertos () {
+function contadorDeAcertos() {
     acertos++;
     primeiraCarta = undefined;
     segundaCarta = undefined;
 
-    if (acertos >= numCartas/2) {
-        setTimeout (finalizarJogo, 1000)
+    if (acertos >= numCartas / 2) {
+        clearInterval(Cronid);
+        setTimeout(finalizarJogo, 1000)
     }
 }
-    
-    
-function finalizarJogo () {
-    alert(`
-Yaay, você ganhou com ${cliques} jogadas, parabéns!
 
-Veja como os parrots estão felizes xD`);
+function finalizarJogo() {
+    
+    alert(`
+Yaay, você ganhou com ${cliques} jogadas e ${temp} segundos, parabéns!
+
+Veja como os parrots estão felizes... 🦜🎉`);
 
     if (confirm("Deseja jogar novamente?")) {
-        window.location.reload(); 
+        window.location.reload();
     }
 }
